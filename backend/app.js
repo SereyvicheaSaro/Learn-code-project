f75cdc3715097a8f30e5de9d65c637437daded9e
SoeurnRotha <kl195070@gmail.com>
2023-03-07 23:22:34 +0700

    build login form

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,47 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const morgan = require('morgan')
 const app = express()
 const url = 'mongodb+srv://Rotha:Rotha@cluster0.hxnopta.mongodb.net/?retryWrites=true&w=majority'
-
-app.get('/', (req,res, next)=>{
-    console.log('hello')
-})
+const userRouter= require('./router/userRouter')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('dev'))
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*")
+    req.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Control-Type, Accept, Authorzation"
+
+    )
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST,PATCH,DELETE' )
+        return res.status(200).json({})
+    }
+    next()
+})
+
+app.use((error, req,res, next) =>{
+    res.status(error.status || 500);
+
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
+async function connect(){
+    try{
+        await mongoose.connect(url)
+        console.log('You connect mongoose successful')
+    }catch(err){
+        console.log(err)
+    }
+}
+connect()
+app.use('/', userRouter )
 module.exports = app
--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -3,7 +3,13 @@ const { default: mongoose } = require('mongoose')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const userModel = require('./../model/userModel')
-router.post('/signUp', (req, res ,next)=>{
+
+router.get('/signup', (req, res, next)=>{
+    res.status(200).json({
+        message: 'You get'
+    })
+})
+router.post('/signup', (req, res ,next)=>{
     bcrypt.hash(req.body.email, 10 , (err, hash)=>{
         if(err){
             return res.status(500).json({
@@ -12,13 +18,27 @@ router.post('/signUp', (req, res ,next)=>{
         }
     })
     const user = new userModel({
-        _id : mongoose.Types.ObjectId(),
-        firstName : req.body.firstName,
+        _id :new mongoose.Types.ObjectId(),
+        fristName : req.body.fristName,
         lastName : req.body.lastName,
         email: req.body.email,
-        password: 
+        password: bcrypt.hash
 
     })
+
+    user.save()
+    then(result =>{
+        res.status(200).json({
+            message : 'You create account successful..',
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            message : "You sign up not found!",
+            error : err
+        })
+    })
+    
 })
 
 module.exports = router
