eba5c5e490f459b54604544c2240c299bb2c5f13
SoeurnRotha <kl195070@gmail.com>
2023-04-02 21:26:29 +0700

    new nav

--- backend/public/javascrip/index.js
+++ backend/public/javascrip/index.js
@@ -13,3 +13,39 @@ const observer = new IntersectionObserver((entries)=>{
 });
 
 hidden.forEach((e)=> observer.observe(e));
+
+
+
+const button_java = document.getElementById('btn-java');
+
+button_java.addEventListener('click', ()=>{
+
+    location.href= './lesson/lessonJava.html';
+
+});
+
+
+const routes = (event) =>{
+    event=event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+
+};
+
+const router ={
+    404 : "",
+    "/" : "/public/index.html",
+    "/course" : "/public/courseList.html",
+    "/login": "public/login.html"
+}
+
+const handleLocation = async ()=>{
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then(
+        (data)=> data.text()
+    );
+};
+window.onpopstate = handleLocation;
+window.route = route;
